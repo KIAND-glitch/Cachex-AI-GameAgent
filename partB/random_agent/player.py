8a7ae12e8134f06cd07de61bf0df64f43bb712df
calvin <qiawen.yu@student.unimelb.edu.au>
2022-04-23 19:27:55 +1000

    random agent

---
+++ partB/random_agent/player.py
@@ -0,0 +1,66 @@
+
+
+
+from random import choice
+from random_agent.board import Board
+
+
+RED = 1
+BLUE = 2
+
+class Player:
+    def __init__(self, player, n):
+        """
+        Called once at the beginning of a game to initialise this player.
+        Set up an internal representation of the game state.
+
+        The parameter player is the string "red" if your player will
+        play as Red, or the string "blue" if your player will play
+        as Blue.
+        """
+        self.n = n
+        self.player = RED if player == "red" else BLUE
+        # n x n array for state
+        self.board = Board(n)
+
+
+    def action(self):
+        """
+        Called at the beginning of your turn. Based on the current state
+        of the game, select an action to play.
+        """
+        action_space = []
+        # add every empty node to action space
+        for i in range(self.n):
+            for j in range(self.n):
+                if not self.board.is_occupied((i, j)):
+                    action_space.append((i, j))
+        # randomly choose action
+        action = choice(action_space)
+        
+        # ignore steal for now
+        return ("PLACE", action[0], action[1])
+
+
+
+    
+    def turn(self, player, action):
+        """
+        Called at the end of each player's turn to inform this player of 
+        their chosen action. Update your internal representation of the 
+        game state based on this. The parameter action is the chosen 
+        action itself. 
+        
+        Note: At the end of your player's turn, the action parameter is
+        the same as what your player returned from the action method
+        above. However, the referee has validated it at this point.
+        """
+        # put your code here
+        if action[0] == "STEAL":
+            for i in range(self.n):
+                for j in range(self.n):
+                    if self.state[i][j] != 0:
+                        self.state[i][j] = BLUE
+
+        if action[0] == "PLACE":
+            self.board.place(player, action[1:])
