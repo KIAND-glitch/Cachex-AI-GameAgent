4f6554191122b993d3f872463ff827fab4f3cfb1
calvin <qiawen.yu@student.unimelb.edu.au>
2022-05-09 18:22:50 +1000

    testing script

--- partB/testing.sh
+++ partB/testing.sh
@@ -6,7 +6,7 @@ do
     echo "size $i" >> results.txt
     for (( j = 1 ; j <= 50; j++ )) ### Inner for loop ###
     do
-        python -m referee -s 100 -t $((i * i)) -l log.txt $i minimax_agent_v2 random_agent
+        python -m referee -s 100 -t $((i * i)) $i minimax_agent_v2 random_agent > log.txt
         tail -n 1 log.txt >> results.txt
     done
 
@@ -20,7 +20,7 @@ do
     echo "size $i" >> results.txt
     for (( j = 1 ; j <= 50; j++ )) ### Inner for loop ###
     do
-        python -m referee -s 100 -t $((i * i)) -l log.txt $i random_agent minimax_agent_v2
+        python -m referee -s 100 -t $((i * i)) $i random_agent minimax_agent_v2 > log.txt
         tail -n 1 log.txt >> results.txt
     done
 
