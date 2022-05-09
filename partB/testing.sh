dc020c09cfd946ee977ac7ca86895c41855a39ea
calvin <qiawen.yu@student.unimelb.edu.au>
2022-05-09 18:04:39 +1000

    testing script

--- partB/testing.sh
+++ partB/testing.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
+echo "RED" >> results.txt
+
 for (( i = 3; i <= 15; i++ ))      ### Outer for loop ###
 do
-
+    echo "size $i" >> results.txt
     for (( j = 1 ; j <= 50; j++ )) ### Inner for loop ###
     do
-        python -m referee -s 100 -t $((i * i)) $i minimax_agent random_agent >> red.txt
+        python -m referee -s 100 -t $((i * i)) -l log.txt $i minimax_agent random_agent
+        echo "Round $j of size $i" >> results.txt
+        tail -n 1 log.txt >> results.txt
     done
 
-  echo "" #### print the new line ###
 done
 
+echo "" >> results.txt
+echo "BLUE" >> results.txt
+
 for (( i = 3; i <= 15; i++ ))      ### Outer for loop ###
 do
-
+    echo "size $i" >> results.txt
     for (( j = 1 ; j <= 50; j++ )) ### Inner for loop ###
     do
-        python -m referee -s 100 -t $((i * i)) $i random_agent minimax_agent >> blue.txt
+        python -m referee -s 100 -t $((i * i)) -l log.txt $i random_agent minimax_agent
+        echo "Round $j of size $i" >> results.txt
+        tail -n 1 log.txt >> results.txt
     done
 
-  echo "" #### print the new line ###
+
 done
